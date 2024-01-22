52c9edf0d3d45a5cbdcdfa44b4b1e0965ee22de8
Doyeon Ryu <ryudoyeonrdy.gmail.com>
2024-01-22 22:11:43 +0900

    Moddified Step2

--- Step2/2480-threeDice.c
+++ Step2/2480-threeDice.c
@@ -1,28 +1,52 @@
+/*
+Problem: 2480
+    세 주사위 값 비교 후 가격 구하기
+Input e.g.:
+    4 4 6
+Output e.g.:
+    1400
+
+Summary of file: 
+    세 주사위를 던져
+    세 숫자가 동일할 때: 10,000 + 동일한 숫자 값 x 1,000(원)
+    두 숫자만 동일할 때: 1,000 + 동일한 숫자 값 x 100(원)
+    겹치지 않을 때: 가장 큰 값 x 100(원) 
+    출력
+
+    주석에 있는 코드:
+        세 가지 변수, 즉 세 주사위에 1-6 사이의 숫자를 무작위로 할당하여 숫자 비교 후 가격 구하기
+    문제 코드:
+        세 주사위 값을 키보드 입력 받아 숫자 비교 후 가격 구하기
+*/
 #include <stdio.h>
 
 /*
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <stdlib.h> // for rand()
+#include <time.h> // for rand()
 
 int main()
 {
     srand(time(NULL));
 
     int a, b, c;
-    a = rand() % 6 + 1;
+    a = rand() % 6 + 1; // 1-6까지 값 할당
     b = rand() % 6 + 1;
     c = rand() % 6 + 1;
 
-    if (a == b && a == c && b == c) {
+    // 세 주사위 값 모두 동일할 때
+    if (a == b && a == c && b == c) { 
         printf("%d\n", 10000 + a * 1000);
     }
+    // 두 주사위 값만 동일할 때
     else if (a == b || a == c) {
         printf("%d\n", 1000 + a * 100);
     }
     else if (b == c) {
         printf("%d\n", 1000 + b * 100);
     }
+
+    // 세 주사위 값 전부 다를 때
     else {
         int max;
         if (a > b && a > c) {
@@ -40,21 +64,26 @@ int main()
     return 0;
 }
 */
+
 int main()
 {
 
     int a, b, c;
-    scanf("%d %d %d", &a, &b, &c);
+    scanf("%d %d %d", &a, &b, &c); // 세 주사위 값 입력 받기
 
+    // 세 주사위 값 모두 동일할 때
     if (a == b && a == c && b == c) {
         printf("%d\n", 10000 + a * 1000);
     }
+    // 두 주사위 값만 동일할 때
     else if (a == b || a == c) {
         printf("%d\n", 1000 + a * 100);
     }
     else if (b == c) {
         printf("%d\n", 1000 + b * 100);
     }
+
+    // 세 주사위 값 전부 다를 때
     else {
         int max;
         if (a > b && a > c) {
--- Step2/2884-alarmClock.c
+++ Step2/2884-alarmClock.c
@@ -1,26 +1,42 @@
+/*
+Problem: 2480
+    현재 시간과 오븐에 구워야 하는 시간의 합을 구해 오븐에서 꺼낼 시간 구하기
+Input e.g.:
+    시 분
+Output e.g.:
+    오븐에 구워야 하는 시간
+
+Summary of file: 
+    현재 시간과 오븐에 구워야 하는 시간의 합을 구할 때 주의할 점:
+        1. 분 60분이 넘으면 시가 그에 맞게 1씩 증가해야 함
+        2. 23시 다음은 0시부터 다시 시작
+
+*/
+
 #include <stdio.h>
-#include <stdlib.h>
 
 int main()
 {
     int a, b, c;
+    int h = 0;
 
     scanf("%d %d\n%d", &a, &b, &c);
 
-    if ((b + c) >= 60) {
-        b = 60 - abs(c - b);
-        if (a == 23) {
-            a = 0;
+    b = b + c;
+
+    if (b >= 60) {
+        while (b >= 60) {
+            b = b - 60;
+            h++;
         }
-        else {
-            a = a + 1; 
-        }
-    } 
-    else if ( (b + c) < 60) {
-        b = b + c;
+        a = a + h;
     }
 
-    printf("%d %d", b, c);
+    if ( a >= 24) {
+        a = a - 24;
+    }
+
+    printf("%d %d\n", a, b);
 
     return 0;
 }
